8b9f620f8d78519cccf09d8661d30cf81836c407
deepaksingh7 <deepaksingh7@gmail.com>
2019-11-22 15:40:19 -0600

    initial commit

---
+++ src/main/java/com/doc/management/DocManagementApplication.java
@@ -0,0 +1,13 @@
+package com.doc.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocManagementApplication.class, args);
+	}
+
+}
